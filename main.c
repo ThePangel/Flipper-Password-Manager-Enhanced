f9ec1ed4356d710f9e7c6a1e5b25d230a7a34253
ThePangel <74447121+ThePangel@users.noreply.github.com>
2025-09-06 15:19:12 +0200

    Expanded functionality

--- main.c
+++ main.c
@@ -3,12 +3,38 @@
 #include <gui/gui.h>
 #include <string.h>
 #include <stdlib.h>
-#include <furi_hal_usb.h>
-#include <furi_hal_usb_hid.h>
 
-#include "flipper_password_manager_enhanced_icons.h"
+#include "password_manager_enhanced_icons.h"
+#define USB_ASCII_TO_KEY(x) (((uint8_t)x < 128) ? (test[(uint8_t)x]) : HID_KEYBOARD_NONE)
 
-#define CSV_LOAD_PATH EXT_PATH("apps_data") "/password_manager_enhanced"
+#define CSV_LOAD_PATH APP_DATA_PATH("")
+#define KBL_LOAD_PATH APP_ASSETS_PATH("")
+
+static void usb_hid_paste(char* str) {
+    furi_hal_usb_enable();
+    furi_hal_usb_unlock();
+
+    furi_check(furi_hal_usb_set_config(&usb_hid, NULL));
+    furi_delay_ms(300);
+    for(size_t i = 0; i < strlen(str); i++) {
+        furi_hal_hid_kb_press(USB_ASCII_TO_KEY(str[i]));
+        furi_hal_hid_kb_release(USB_ASCII_TO_KEY(str[i]));
+        furi_delay_ms(15);
+    }
+}
+
+static void ble_hid_paste(void* context, char* str) {
+    AppContext* app = context;
+
+    furi_delay_ms(300);
+
+    for(size_t i = 0; i < strlen(str); i++) {
+        ble_profile_hid_kb_press(app->ble, USB_ASCII_TO_KEY(str[i]));
+        ble_profile_hid_kb_release(app->ble, USB_ASCII_TO_KEY(str[i]));
+
+        furi_delay_ms(15);
+    }
+}
 
 static void add_to_queue_callback(const void* input, void* context) {
     const InputEvent* event = input;
@@ -18,7 +44,8 @@ static void add_to_queue_callback(const void* input, void* context) {
 
 void app_handle_csv(void* context) {
     AppContext* app = context;
-    if(!((app->file_name == app->file_path) && app->entry_count > 0)) {
+
+    if(!(furi_string_cmp(app->file_name, app->file_path) == 0 && app->entry_count > 0)) {
         Storage* storage = furi_record_open(RECORD_STORAGE);
         File* file = storage_file_alloc(storage);
         size_t count = 0;
@@ -118,7 +145,7 @@ void app_handle_csv(void* context) {
         }
 
         app->entry_count = count;
-        app->file_name = app->file_path;
+        furi_string_set(app->file_name, app->file_path);
         storage_file_close(file);
         storage_file_free(file);
         furi_record_close(RECORD_STORAGE);
@@ -133,7 +160,17 @@ bool app_scene_on_event_main_menu(void* context, SceneManagerEvent event) {
     case SceneManagerEventTypeCustom:
         switch(event.event) {
         case AppEvent_FileBrowser:
-            scene_manager_next_scene(app->scene_manager, AppScene_Submenu);
+            DialogsFileBrowserOptions browser_options;
+            dialog_file_browser_set_basic_options(&browser_options, ".csv", &I_u2f_10px);
+            browser_options.hide_dot_files = true;
+            browser_options.hide_ext = false;
+            browser_options.base_path = CSV_LOAD_PATH;
+            furi_string_set(app->file_path, browser_options.base_path);
+            if(dialog_file_browser_show(
+                   app->dialogs, app->file_path, app->file_path, &browser_options)) {
+                scene_manager_next_scene(app->scene_manager, AppScene_loading);
+                app_handle_csv(app);
+            }
             consumed = true;
             break;
         }
@@ -190,9 +227,31 @@ bool app_scene_on_event_loading_screen(void* context, SceneManagerEvent event) {
 
 bool app_scene_on_event_password_menu(void* context, SceneManagerEvent event) {
     AppContext* app = context;
-    UNUSED(app);
     bool consumed = false;
     switch(event.type) {
+    case SceneManagerEventTypeCustom:
+        switch(event.event) {
+        case AppEvent_PastePasswrd:
+            char* user = app->entry[app->entry_index].username;
+            char* pass = app->entry[app->entry_index].password;
+            if(app->using_ble) {
+                ble_hid_paste(app, user);
+                ble_profile_hid_kb_press(app->ble, HID_KEYBOARD_TAB);
+                ble_profile_hid_kb_release(app->ble, HID_KEYBOARD_TAB);
+                ble_hid_paste(app, pass);
+            } else {
+                usb_hid_paste(user);
+                furi_hal_hid_kb_press(HID_KEYBOARD_TAB);
+                furi_hal_hid_kb_release(HID_KEYBOARD_TAB);
+                usb_hid_paste(pass);
+            }
+            consumed = true;
+            break;
+        default:
+            break;
+        }
+        break;
+
     default:
         consumed = false;
         break;
@@ -204,18 +263,11 @@ void app_menu_callback_main_menu(void* context, uint32_t index) {
     AppContext* app = context;
     switch(index) {
     case 0:
+        view_dispatcher_send_custom_event(app->view_dispatcher, AppEvent_FileBrowser);
 
-        DialogsFileBrowserOptions browser_options;
-        dialog_file_browser_set_basic_options(&browser_options, ".csv", &I_u2f_10px);
-        browser_options.hide_dot_files = true;
-        browser_options.hide_ext = false;
-        browser_options.base_path = CSV_LOAD_PATH;
-        furi_string_set(app->file_path, browser_options.base_path);
-        if(dialog_file_browser_show(
-               app->dialogs, app->file_path, app->file_path, &browser_options)) {
-            scene_manager_next_scene(app->scene_manager, AppScene_loading);
-            app_handle_csv(app);
-        }
+        break;
+    case 1:
+        scene_manager_next_scene(app->scene_manager, AppScene_options);
         break;
     }
 }
@@ -224,22 +276,56 @@ void app_dialog_username_callback(DialogExResult result, void* context) {
     AppContext* app = context;
     switch(result) {
     case DialogExResultLeft:
-        view_dispatcher_switch_to_view(app->view_dispatcher, AppView_Submenu);
+        view_dispatcher_switch_to_view(app->view_dispatcher, AppView_password_menu);
         break;
     case DialogExResultCenter:
-        furi_hal_usb_enable();
-        furi_hal_usb_unlock();
-        furi_check(furi_hal_usb_set_config(&usb_hid, NULL));
-        furi_delay_ms(100);
-        fuir_hal_ char* str = app->entry[app->entry_index].username;
-        for(size_t i = 0; i < strlen(str); i++) {
-            furi_hal_hid_kb_press(HID_ASCII_TO_KEY(str[i]));
-            furi_hal_hid_kb_release(HID_ASCII_TO_KEY(str[i]));
-            furi_delay_ms(10);
-        }
+        char* str = app->entry[app->entry_index].username;
+        if(app->using_ble)
+            ble_hid_paste(app, str);
+        else
+            usb_hid_paste(str);
+
+        break;
+
+    case DialogExResultRight:
+        app_menu_password_dialog_callback(app, app->entry_index);
+        break;
     default:
         break;
     }
+}
+
+void app_dialog_password_callback(DialogExResult result, void* context) {
+    AppContext* app = context;
+    switch(result) {
+    case DialogExResultLeft:
+        app_menu_username_dialog_callback(app, app->entry_index);
+        break;
+    case DialogExResultCenter:
+
+        char* str = app->entry[app->entry_index].password;
+        if(app->using_ble)
+            ble_hid_paste(app, str);
+        else
+            usb_hid_paste(str);
+
+        break;
+    default:
+        break;
+    }
+}
+void app_menu_password_dialog_callback(void* context, uint32_t index) {
+    AppContext* app = context;
+    dialog_ex_reset(app->dialog);
+    dialog_ex_set_header(app->dialog, "Password:", 64, 16, AlignCenter, AlignCenter);
+    dialog_ex_set_text(app->dialog, app->entry[index].password, 64, 32, AlignCenter, AlignCenter);
+    dialog_ex_set_left_button_text(app->dialog, "Back");
+    dialog_ex_set_center_button_text(app->dialog, "Paste");
+
+    dialog_ex_set_context(app->dialog, app);
+    dialog_ex_set_result_callback(app->dialog, app_dialog_password_callback);
+    app->entry_index = index;
+    view_dispatcher_switch_to_view(app->view_dispatcher, AppView_dialog);
 }
 
 void app_menu_username_dialog_callback(void* context, uint32_t index) {
@@ -249,11 +335,17 @@ void app_menu_username_dialog_callback(void* context, uint32_t index) {
     dialog_ex_set_text(app->dialog, app->entry[index].username, 64, 32, AlignCenter, AlignCenter);
     dialog_ex_set_left_button_text(app->dialog, "Back");
     dialog_ex_set_center_button_text(app->dialog, "Paste");
-    dialog_ex_set_right_button_text(app->dialog, "Paswd");
+    dialog_ex_set_right_button_text(app->dialog, "Pasw");
     dialog_ex_set_context(app->dialog, app);
     dialog_ex_set_result_callback(app->dialog, app_dialog_username_callback);
     app->entry_index = index;
     view_dispatcher_switch_to_view(app->view_dispatcher, AppView_dialog);
+}
+
+void app_menu_paste_callback(void* context, uint32_t index) {
+    AppContext* app = context;
+    app->entry_index = index - 2;
+    view_dispatcher_send_custom_event(app->view_dispatcher, AppEvent_PastePasswrd);
 }
 
 void app_menu_setup_password_menu(void* context, uint32_t index) {
@@ -264,7 +356,9 @@ void app_menu_setup_password_menu(void* context, uint32_t index) {
     char username[64];
     snprintf(username, sizeof(username), "Username: %s", app->entry[index].username);
     submenu_add_item(app->password_menu, username, index, app_menu_username_dialog_callback, app);
-    submenu_add_item(app->password_menu, "Reveal password", index + 1, NULL, app);
+    submenu_add_item(
+        app->password_menu, "Reveal password", index + 1, app_menu_password_dialog_callback, app);
+    submenu_add_item(app->password_menu, "Paste", index + 2, app_menu_paste_callback, app);
     view_dispatcher_switch_to_view(app->view_dispatcher, AppView_password_menu);
 }
 
@@ -272,6 +366,8 @@ void app_scene_on_enter_main_menu(void* context) {
     AppContext* app = context;
     submenu_reset(app->menu);
     submenu_add_item(app->menu, "View passwords", 0, app_menu_callback_main_menu, app);
+    submenu_add_item(app->menu, "Options", 1, app_menu_callback_main_menu, app);
+    submenu_add_item(app->menu, "About", 2, app_menu_callback_main_menu, app);
     view_dispatcher_switch_to_view(app->view_dispatcher, AppView_MainSMenu);
 }
 void app_scene_on_enter_sub_menu(void* context) {
@@ -294,7 +390,6 @@ void app_scene_on_enter_loading_screen(void* context) {
 void app_scene_on_enter_password_menu(void* context) {
     AppContext* app = context;
     submenu_reset(app->password_menu);
-    scene_manager_handle_custom_event(app->scene_manager, AppEvent_LoadPasswrd);
 }
 
 void app_scene_on_exit_sub_menu(void* context) {
@@ -317,11 +412,68 @@ void app_scene_on_exit_password_menu(void* context) {
     submenu_reset(app->password_menu);
 }
 
+void app_options_callback(void* context, uint32_t index) {
+    AppContext* app = context;
+
+    switch(index) {
+    case 1:
+        view_dispatcher_send_custom_event(app->view_dispatcher, AppEvent_FileBrowser);
+
+    default:
+        break;
+    }
+}
+
+void app_scene_on_enter_options(void* context) {
+    AppContext* app = context;
+    variable_item_list_reset(app->options);
+    VariableItem* bluetooth = variable_item_list_add(app->options, "Bluetooth", 2, NULL, app);
+    VariableItem* set_kbl = variable_item_list_add(app->options, "Keyboard Layout", 1, NULL, app);
+    UNUSED(bluetooth);
+    UNUSED(set_kbl);
+
+    view_dispatcher_switch_to_view(app->view_dispatcher, AppView_options);
+}
+
+bool app_scene_on_event_options(void* context, SceneManagerEvent event) {
+    AppContext* app = context;
+    bool consumed = false;
+    switch(event.type) {
+    case SceneManagerEventTypeCustom:
+        switch(event.event) {
+        case AppEvent_FileBrowser:
+            DialogsFileBrowserOptions browser_options;
+            dialog_file_browser_set_basic_options(&browser_options, ".kl", &I_keyboard_10px);
+            browser_options.hide_dot_files = true;
+            browser_options.hide_ext = false;
+            browser_options.base_path = KBL_LOAD_PATH;
+            furi_string_set(app->file_path, browser_options.base_path);
+            if(dialog_file_browser_show(
+                   app->dialogs, app->file_path, app->file_path, &browser_options)) {
+                scene_manager_next_scene(app->scene_manager, AppScene_options);
+            }
+            break;
+        }
+        break;
+
+    default:
+        consumed = false;
+        break;
+    }
+    return consumed;
+}
+
+void app_scene_on_exit_options(void* context) {
+    AppContext* app = context;
+    variable_item_list_reset(app->options);
+}
+
 void (*const app_scene_on_enter_handlers[])(void*) = {
     app_scene_on_enter_main_menu,
     app_scene_on_enter_sub_menu,
     app_scene_on_enter_loading_screen,
     app_scene_on_enter_password_menu,
+    app_scene_on_enter_options,
 };
 
 bool (*const app_scene_on_event_handlers[])(void*, SceneManagerEvent) = {
@@ -329,6 +481,7 @@ bool (*const app_scene_on_event_handlers[])(void*, SceneManagerEvent) = {
     app_scene_on_event_sub_menu,
     app_scene_on_event_loading_screen,
     app_scene_on_event_password_menu,
+    app_scene_on_event_options,
 
 };
 
@@ -337,6 +490,7 @@ void (*const app_scene_on_exit_handlers[])(void*) = {
     app_scene_on_exit_sub_menu,
     app_scene_on_exit_loading_screen,
     app_scene_on_exit_password_menu,
+    app_scene_on_exit_options,
 };
 
 const SceneManagerHandlers app_scene_event_handlers = {
@@ -357,10 +511,10 @@ bool app_back_event_callback(void* context) {
 
 AppContext* app_alloc() {
     AppContext* app = malloc(sizeof(AppContext));
-
+    app->bt = furi_record_open(RECORD_BT);
     app->scene_manager = scene_manager_alloc(&app_scene_event_handlers, app);
     app->view_dispatcher = view_dispatcher_alloc();
-
+    app->using_ble = false;
     view_dispatcher_set_event_callback_context(app->view_dispatcher, app);
     view_dispatcher_set_custom_event_callback(app->view_dispatcher, app_scene_custom_callback);
     view_dispatcher_set_navigation_event_callback(app->view_dispatcher, app_back_event_callback);
@@ -377,6 +531,10 @@ AppContext* app_alloc() {
     app->dialog = dialog_ex_alloc();
     view_dispatcher_add_view(
         app->view_dispatcher, AppView_dialog, dialog_ex_get_view(app->dialog));
+    app->options = variable_item_list_alloc();
+    view_dispatcher_add_view(
+        app->view_dispatcher, AppView_options, variable_item_list_get_view(app->options));
+    variable_item_list_set_enter_callback(app->options, app_options_callback, app);
 
     app->dialogs = furi_record_open(RECORD_DIALOGS);
     Storage* storage = furi_record_open(RECORD_STORAGE);
@@ -390,6 +548,8 @@ AppContext* app_alloc() {
 
 void app_free(void* context) {
     AppContext* app = context;
+    furi_hal_usb_set_config(&usb_cdc_single, NULL);
+    furi_hal_usb_lock();
 
     scene_manager_free(app->scene_manager);
 
@@ -398,6 +558,7 @@ void app_free(void* context) {
     view_dispatcher_remove_view(app->view_dispatcher, AppView_Loading);
     view_dispatcher_remove_view(app->view_dispatcher, AppView_password_menu);
     view_dispatcher_remove_view(app->view_dispatcher, AppView_dialog);
+    view_dispatcher_remove_view(app->view_dispatcher, AppView_options);
 
     view_dispatcher_free(app->view_dispatcher);
 
@@ -406,6 +567,7 @@ void app_free(void* context) {
     widget_free(app->loading);
     submenu_free(app->password_menu);
     dialog_ex_free(app->dialog);
+    variable_item_list_free(app->options);
     furi_record_close(RECORD_DIALOGS);
     furi_string_free(app->file_path);
     furi_string_free(app->file_name);
@@ -417,6 +579,8 @@ void app_free(void* context) {
         }
         free(app->entry);
     }
+    furi_record_close(RECORD_BT);
+    app->bt = NULL;
     free(app);
 }
 
@@ -425,9 +589,40 @@ int32_t flipper_pass_manager_app(void* p) {
 
     AppContext* app = app_alloc();
     Gui* gui = furi_record_open(RECORD_GUI);
+
+    Storage* storage = furi_record_open(RECORD_STORAGE);
+
+    storage_common_migrate(
+        storage,
+        EXT_PATH("apps/Bluetooth/" HID_BT_KEYS_STORAGE_NAME),
+        APP_DATA_PATH(HID_BT_KEYS_STORAGE_NAME));
+
+    bt_disconnect(app->bt);
+
+    furi_delay_ms(200);
+
+    bt_keys_storage_set_storage_path(app->bt, APP_DATA_PATH(HID_BT_KEYS_STORAGE_NAME));
+
+    furi_record_close(RECORD_STORAGE);
+
+    app->ble = bt_profile_start(app->bt, ble_profile_hid, NULL);
+    if(!app->ble) {
+        FURI_LOG_E("FPME", "Failed to start BLE profile");
+    }
+
+    furi_hal_bt_start_advertising();
     view_dispatcher_attach_to_gui(app->view_dispatcher, gui, ViewDispatcherTypeFullscreen);
     scene_manager_next_scene(app->scene_manager, AppScene_MainMenu);
     view_dispatcher_run(app->view_dispatcher);
+    bt_disconnect(app->bt);
+
+    furi_delay_ms(200);
+
+    bt_keys_storage_set_default_path(app->bt);
+
+    if(!bt_profile_restore_default(app->bt)) {
+        FURI_LOG_E("FPME", "Failed to switch to Serial profile");
+    }
 
     app_free(app);
     furi_record_close(RECORD_GUI);
--- main.h
+++ main.h
@@ -5,17 +5,26 @@
 #include <gui/view_dispatcher.h>
 #include <gui/scene_manager.h>
 #include <gui/modules/menu.h>
+#include <gui/modules/variable_item_list.h>
 #include <gui/modules/submenu.h>
 #include <gui/modules/dialog_ex.h>
 #include <gui/modules/widget.h>
 #include <dialogs/dialogs.h>
 #include <storage/storage.h>
+#include <furi_hal_bt.h>
+#include <extra_profiles/hid_profile.h>
+#include <bt/bt_service/bt.h>
+#include <furi_hal_usb.h>
+#include <furi_hal_usb_hid.h>
+
+#define HID_BT_KEYS_STORAGE_NAME ".bt_hid.keys"
 
 typedef enum {
     AppScene_MainMenu,
     AppScene_Submenu,
     AppScene_loading,
     AppScene_password_menu,
+    AppScene_options,
     AppScene_count,
 
 } AppScene;
@@ -35,9 +44,13 @@ typedef struct {
     Widget* loading;
     DialogEx* dialog;
     DialogsApp* dialogs;
+    VariableItemList* options;
     FuriString* file_path;
     FuriString* file_name;
     CsvEntry* entry;
+    Bt* bt;
+    FuriHalBleProfileBase* ble;
+    bool using_ble;
     size_t entry_count;
     uint16_t entry_index;
 
@@ -49,9 +62,144 @@ typedef enum {
     AppView_Loading,
     AppView_password_menu,
     AppView_dialog,
+    AppView_options,
 } AppView;
 
 typedef enum {
     AppEvent_FileBrowser,
-    AppEvent_LoadPasswrd,
+    AppEvent_PastePasswrd,
 } AppEvent;
+
+const uint16_t test[] = {
+    HID_KEYBOARD_NONE, // NUL
+    HID_KEYBOARD_NONE, // SOH
+    HID_KEYBOARD_NONE, // STX
+    HID_KEYBOARD_NONE, // ETX
+    HID_KEYBOARD_NONE, // EOT
+    HID_KEYBOARD_NONE, // ENQ
+    HID_KEYBOARD_NONE, // ACK
+    HID_KEYBOARD_NONE, // BEL
+    HID_KEYBOARD_DELETE, // BS   Backspace
+    HID_KEYBOARD_TAB, // TAB  Tab
+    HID_KEYBOARD_RETURN, // LF   Enter
+    HID_KEYBOARD_NONE, // VT
+    HID_KEYBOARD_NONE, // FF
+    HID_KEYBOARD_NONE, // CR
+    HID_KEYBOARD_NONE, // SO
+    HID_KEYBOARD_NONE, // SI
+    HID_KEYBOARD_NONE, // DEL
+    HID_KEYBOARD_NONE, // DC1
+    HID_KEYBOARD_NONE, // DC2
+    HID_KEYBOARD_NONE, // DC3
+    HID_KEYBOARD_NONE, // DC4
+    HID_KEYBOARD_NONE, // NAK
+    HID_KEYBOARD_NONE, // SYN
+    HID_KEYBOARD_NONE, // ETB
+    HID_KEYBOARD_NONE, // CAN
+    HID_KEYBOARD_NONE, // EM
+    HID_KEYBOARD_NONE, // SUB
+    HID_KEYBOARD_NONE, // ESC
+    HID_KEYBOARD_NONE, // FS
+    HID_KEYBOARD_NONE, // GS
+    HID_KEYBOARD_NONE, // RS
+    HID_KEYBOARD_NONE, // US
+    HID_KEYBOARD_SPACEBAR, // ' ' Space
+    HID_KEYBOARD_1 | KEY_MOD_LEFT_SHIFT, // !
+    HID_KEYBOARD_APOSTROPHE | KEY_MOD_LEFT_SHIFT, // "
+    HID_KEYBOARD_3 | KEY_MOD_RIGHT_ALT, // #
+    HID_KEYBOARD_4 | KEY_MOD_LEFT_SHIFT, // $
+    HID_KEYBOARD_5 | KEY_MOD_LEFT_SHIFT, // %
+    HID_KEYBOARD_7 | KEY_MOD_LEFT_SHIFT, // &
+    HID_KEYBOARD_APOSTROPHE, // '
+    HID_KEYBOARD_9 | KEY_MOD_LEFT_SHIFT, // (
+    HID_KEYBOARD_0 | KEY_MOD_LEFT_SHIFT, // )
+    HID_KEYBOARD_8 | KEY_MOD_LEFT_SHIFT, // *
+    HID_KEYBOARD_EQUAL_SIGN | KEY_MOD_LEFT_SHIFT, // +
+    HID_KEYBOARD_COMMA, // ,
+    HID_KEYBOARD_MINUS, // -
+    HID_KEYBOARD_DOT, // .
+    HID_KEYBOARD_7 | KEY_MOD_LEFT_SHIFT, // /
+    HID_KEYBOARD_0, // 0
+    HID_KEYBOARD_1, // 1
+    HID_KEYBOARD_2, // 2
+    HID_KEYBOARD_3, // 3
+    HID_KEYBOARD_4, // 4
+    HID_KEYBOARD_5, // 5
+    HID_KEYBOARD_6, // 6
+    HID_KEYBOARD_7, // 7
+    HID_KEYBOARD_8, // 8
+    HID_KEYBOARD_9, // 9
+    HID_KEYBOARD_SEMICOLON | KEY_MOD_LEFT_SHIFT, // :
+    HID_KEYBOARD_SEMICOLON, // ;
+    HID_KEYBOARD_COMMA | KEY_MOD_LEFT_SHIFT, // <
+    HID_KEYBOARD_EQUAL_SIGN, // =
+    HID_KEYBOARD_DOT | KEY_MOD_LEFT_SHIFT, // >
+    HID_KEYBOARD_SLASH | KEY_MOD_LEFT_SHIFT, // ?
+    HID_KEYBOARD_2 | KEY_MOD_RIGHT_ALT, // @
+    HID_KEYBOARD_A | KEY_MOD_LEFT_SHIFT, // A
+    HID_KEYBOARD_B | KEY_MOD_LEFT_SHIFT, // B
+    HID_KEYBOARD_C | KEY_MOD_LEFT_SHIFT, // C
+    HID_KEYBOARD_D | KEY_MOD_LEFT_SHIFT, // D
+    HID_KEYBOARD_E | KEY_MOD_LEFT_SHIFT, // E
+    HID_KEYBOARD_F | KEY_MOD_LEFT_SHIFT, // F
+    HID_KEYBOARD_G | KEY_MOD_LEFT_SHIFT, // G
+    HID_KEYBOARD_H | KEY_MOD_LEFT_SHIFT, // H
+    HID_KEYBOARD_I | KEY_MOD_LEFT_SHIFT, // I
+    HID_KEYBOARD_J | KEY_MOD_LEFT_SHIFT, // J
+    HID_KEYBOARD_K | KEY_MOD_LEFT_SHIFT, // K
+    HID_KEYBOARD_L | KEY_MOD_LEFT_SHIFT, // L
+    HID_KEYBOARD_M | KEY_MOD_LEFT_SHIFT, // M
+    HID_KEYBOARD_N | KEY_MOD_LEFT_SHIFT, // N
+    HID_KEYBOARD_O | KEY_MOD_LEFT_SHIFT, // O
+    HID_KEYBOARD_P | KEY_MOD_LEFT_SHIFT, // P
+    HID_KEYBOARD_Q | KEY_MOD_LEFT_SHIFT, // Q
+    HID_KEYBOARD_R | KEY_MOD_LEFT_SHIFT, // R
+    HID_KEYBOARD_S | KEY_MOD_LEFT_SHIFT, // S
+    HID_KEYBOARD_T | KEY_MOD_LEFT_SHIFT, // T
+    HID_KEYBOARD_U | KEY_MOD_LEFT_SHIFT, // U
+    HID_KEYBOARD_V | KEY_MOD_LEFT_SHIFT, // V
+    HID_KEYBOARD_W | KEY_MOD_LEFT_SHIFT, // W
+    HID_KEYBOARD_X | KEY_MOD_LEFT_SHIFT, // X
+    HID_KEYBOARD_Y | KEY_MOD_LEFT_SHIFT, // Y
+    HID_KEYBOARD_Z | KEY_MOD_LEFT_SHIFT, // Z
+    HID_KEYBOARD_OPEN_BRACKET, // [
+    HID_KEYBOARD_BACKSLASH, // bslash
+    HID_KEYBOARD_CLOSE_BRACKET, // ]
+    HID_KEYBOARD_6 | KEY_MOD_LEFT_SHIFT, // ^
+    HID_KEYBOARD_MINUS | KEY_MOD_LEFT_SHIFT, // _
+    HID_KEYBOARD_GRAVE_ACCENT, // `
+    HID_KEYBOARD_A, // a
+    HID_KEYBOARD_B, // b
+    HID_KEYBOARD_C, // c
+    HID_KEYBOARD_D, // d
+    HID_KEYBOARD_E, // e
+    HID_KEYBOARD_F, // f
+    HID_KEYBOARD_G, // g
+    HID_KEYBOARD_H, // h
+    HID_KEYBOARD_I, // i
+    HID_KEYBOARD_J, // j
+    HID_KEYBOARD_K, // k
+    HID_KEYBOARD_L, // l
+    HID_KEYBOARD_M, // m
+    HID_KEYBOARD_N, // n
+    HID_KEYBOARD_O, // o
+    HID_KEYBOARD_P, // p
+    HID_KEYBOARD_Q, // q
+    HID_KEYBOARD_R, // r
+    HID_KEYBOARD_S, // s
+    HID_KEYBOARD_T, // t
+    HID_KEYBOARD_U, // u
+    HID_KEYBOARD_V, // v
+    HID_KEYBOARD_W, // w
+    HID_KEYBOARD_X, // x
+    HID_KEYBOARD_Y, // y
+    HID_KEYBOARD_Z, // z
+    HID_KEYBOARD_OPEN_BRACKET | KEY_MOD_LEFT_SHIFT, // {
+    HID_KEYBOARD_BACKSLASH | KEY_MOD_LEFT_SHIFT, // |
+    HID_KEYBOARD_CLOSE_BRACKET | KEY_MOD_LEFT_SHIFT, // }
+    HID_KEYBOARD_GRAVE_ACCENT | KEY_MOD_LEFT_SHIFT, // ~
+    HID_KEYBOARD_NONE, // DEL
+};
+
+void app_menu_password_dialog_callback(void* context, uint32_t index);
+void app_menu_username_dialog_callback(void* context, uint32_t index);
